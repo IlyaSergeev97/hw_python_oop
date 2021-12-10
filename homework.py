23cbc2dfcb34d64cb8ba0aa4b47b46457b4ef7ec
Илья Сергеев <iluxa.sergeeff@yandex.ru>
2021-12-10 18:57:08 +0300

    1. Добавлена анотация типов 2. Удалены лишние переменные 3. Числа вынесены в константы 4. Добавлен NotImplementedError

--- homework.py
+++ homework.py
@@ -23,8 +23,8 @@ class InfoMessage:
 
 class Training:
     """Базовый класс тренировки."""
-    LEN_STEP = 0.65
-    M_IN_KM = 1000
+    LEN_STEP: float = 0.65
+    M_IN_KM: int = 1000
 
     def __init__(self,
                  action: int,
@@ -44,32 +44,43 @@ class Training:
         return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
+        raise NotImplementedError
         """Получить количество затраченных калорий."""
-        pass
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        message = InfoMessage(training_type=self.__class__.__name__,
-                              duration=self.duration,
-                              distance=self.get_distance(),
-                              speed=self.get_mean_speed(),
-                              calories=self.get_spent_calories())
-        return message
+        return InfoMessage(training_type=self.__class__.__name__,
+                           duration=self.duration,
+                           distance=self.get_distance(),
+                           speed=self.get_mean_speed(),
+                           calories=self.get_spent_calories())
 
 
 class Running(Training):
     """Тренировка: бег."""
-    def __init__(self, action: int, duration: float, weight: float) -> None:
+    Сoefficient1: int = 18
+    Сoefficient2: int = 20
+    S_IN_M: int = 60
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float) -> None:
         super().__init__(action, duration, weight)
 
     def get_spent_calories(self) -> float:
         """Переопределяет метод get_spent_calories."""
-        return ((18 * self.get_mean_speed() - 20) * self.weight
-                / self.M_IN_KM * (self.duration * 60))
+        return ((self.Сoefficient1 * self.get_mean_speed() - self.Сoefficient2)
+                * self.weight / self.M_IN_KM * (self.duration * self.S_IN_M))
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
+    Сoefficient1: float = 0.035
+    Сoefficient2: float = 0.029
+    Exponentiation: int = 2
+    S_IN_M: int = 60
+
     def __init__(self,
                  action: int,
                  duration: float,
@@ -80,14 +91,17 @@ class SportsWalking(Training):
 
     def get_spent_calories(self) -> float:
         """Переопределяет метод get_spent_calories."""
-        return ((0.035 * self.weight
-                + (self.get_mean_speed() ** 2 // self.height)
-                * 0.029 * self.weight) * (self.duration * 60))
+        return ((self.Сoefficient1 * self.weight
+                + (self.get_mean_speed() ** self.Exponentiation // self.height)
+                * self.Сoefficient2 * self.weight)
+                * (self.duration * self.S_IN_M))
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    LEN_STEP = 1.38
+    LEN_STEP: float = 1.38
+    Сoefficient1: float = 1.1
+    Сoefficient2: int = 2
 
     def __init__(self,
                  action: int,
@@ -106,7 +120,8 @@ class Swimming(Training):
 
     def get_spent_calories(self) -> float:
         """Переопределяет метод get_spent_calories."""
-        return (self.get_mean_speed() + 1.1) * 2 * self.weight
+        return ((self.get_mean_speed() + self.Сoefficient1)
+                * self.Сoefficient2 * self.weight)
 
 
 def read_package(workout_type: str, data: list) -> Training:
@@ -114,14 +129,14 @@ def read_package(workout_type: str, data: list) -> Training:
     workouts = {'SWM': Swimming,
                 'RUN': Running,
                 'WLK': SportsWalking}
+
     training = workouts[workout_type](*data)
     return training
 
 
 def main(training: Training) -> None:
     """Главная функция."""
-    info = training.show_training_info()
-    print(info.get_message())
+    print(training.show_training_info().get_message())
 
 
 if __name__ == '__main__':
